87eac19b4243f1e1e3609ac4fd53c03b08c0a582
Ryan Diaz <ryandiaz@Anonymouss-MacBook.local>
2014-02-26 14:12:35 -0800

    end of night tuesday

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 import nltk
 import random
+from nltk.corpus import abc
+from nltk.corpus import gutenberg
+from nltk.corpus import genesis
+from nltk.corpus import webtext
+
+from nltk.probability import LidstoneProbDist  
+from nltk.model import NgramModel 
 
 def loadList(file_name):
     """Loads text files as lists of lines. Used in evaluation."""
@@ -19,6 +26,13 @@ def decodeLine(lines):
     return dict
 
 
+def trainModel():
+    totalwords = abc.words() #+ genesis.words() + gutenberg.words() + webtext.words()
+    estimator = lambda fdist, bins: LidstoneProbDist(fdist, 0.2)
+    BigramModel = NgramModel(2, totalwords)
+    print 'bigram done training'
+    UnigramModel = NgramModel(1, totalwords)
+    return (UnigramModel, BigramModel)
 #def tokenize(doc):
    # nltk.tokenize
 
@@ -46,6 +60,8 @@ def main():
     in_file = "./dev_set.txt"
     doc = loadList(in_file)
 
+    (UnigramModel, BigramModel) = trainModel()
+
     nouns = ['PRP', 'PRP$', 'NNP', 'NNPS', 'NNS', 'NN']
     adjectives = ['JJR', 'JJS', 'JJ']
     adverbs = ['RB', 'RBS', 'RBR']
@@ -61,44 +77,74 @@ def main():
             word = word.replace(",", "")
             word = word.replace("?", "")
             word = word.replace("!", "")
+            word = word.replace(";", "")
             word = word.lower()
             spanWords.append(word)
 
-        for tsize in range(2,4):
-            for i in range(0, len(spanWords)-tsize+1, tsize ):
+        for tsize in range(4,1,-1):#range(2,5):
+            for i in range(0, len(spanWords)-tsize+1):
                 phrase = ' '.join(spanWords[i:i+tsize])
                 if(phrase in revDict):
                     spanWords[i] = phrase
-                    newWords[i+1:i+tsize] = [""]*(tsize-1)
 
+                    # newWords[i+1:i+tsize] = [""]*(tsize-1)
+        position = 0
         for word in spanWords:
             if word in revDict:
                 nw = revDict[word][0]
                 if len(revDict[word]) > 1:
-                    nw = random.choice(revDict[word])
-                newWords.append(nw)
+                    maxW = ''
+                    maxP = 0.0
+                    print word, '::>', revDict[word]
+                    if position > 0:
+                        
+                        for ambW in revDict[word]:
+
+                            p = BigramModel.prob(ambW,[newWords[position]])
+                            # print ambW, newWords[position], p
+                            if p > maxP:
+                                maxP = p
+                                maxW = ambW
+                        print maxW
+                    else:
+                        for ambW in revDict[word]:
+
+                            p = UnigramModel.prob(ambW,[])
+                            if p > maxP:
+                                p = maxP
+                                maxW = ambW
+                    nw = maxW #random.choice(revDict[word])
+
+                if len(nw.split()) > 1:
+                    nws = nw.split()
+                    for w in nws: newWords.append(w)
+                else:    
+                    newWords.append(nw)
+                position += 1
+        newWords = filter(lambda a: a != '', newWords)
         posTags = nltk.pos_tag(newWords)
-        # print posTags
+        print posTags
         # print newWords
         # print 'diff in length= ', len(posTags) - len(newWords)
         for i in range(0, len(posTags) - 1):
-            print i
+            # print i
             
             if i < len(posTags) - 2:
                 if posTags[i][1] in nouns and posTags[i+1][1] in adverbs and posTags[i+2][1] in adjectives:
                     buf = newWords[i]
-                    print posTags[i][0]
-                    print newWords[i] 
+                    # print posTags[i][0]
+                    # print newWords[i] 
                     # print 'deleting : ', buf
                     del newWords[i]
                     newWords.insert(i+2,buf)
                     # print 'inserted at ', i+2
-
+                if posTags[i][1] in nouns and posTags[i+1][0] == 'of' and posTags[i+2][1] in adjectives:
+                    newWords[i:i+3] = [posTags[i+2][0], '' ,posTags[i][0]]
             if i < len(posTags) - 3:
                 if posTags[i][1] in nouns and posTags[i+1][1] in adjectives and posTags[i+2][1] in conjunctions and posTags[i+3][1] in adjectives:
                     buf = newWords[i]
-                    print posTags[i][0]
-                    print newWords[i]
+                    # print posTags[i][0]
+                    # print newWords[i]
                     # print 'deleting: ', buf
                     del newWords[i]
                     newWords.insert(i+3,buf)
@@ -114,6 +160,8 @@ def main():
 
             # else:
             #     newWords.append("nothing_yet")
+        
+        newWords = filter(lambda a: a != '', newWords)
         print ' '.join(newWords)
         print '  ' 
         sentence = ""
